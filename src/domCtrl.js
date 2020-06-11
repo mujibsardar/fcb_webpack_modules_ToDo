871022ff4c37609281be24bc8d7dadcd4c605e13
Avan <mujibsardar@gmail.com>
2020-06-10 19:02:08 -0700

    add todo form

--- src/domCtrl.js
+++ src/domCtrl.js
@@ -1,9 +1,47 @@
 const domCtrl = (() => {
-	const init = () => {
-	   console.log("Hi from init in domCtrl.js")
+	// Call on this function to initialize all dom related elements
+	const init = (toDoArray) => {
+	   console.log("Hi from init in domCtrl.js");
+		 // Delete old content
+		 resetContainer();
+		 // Setup the new todo form
+		 setupPage();
+		 // Now display all the todo elements
+		 //displayToDoList(toDoArray);
 	};
-	return {
-		init
-	};
+
+	// Delete all the content of the main div with the id 'content'
+	const resetContainer = () => {
+		let root_element = document.getElementById("content");
+		root_element.innerHTML = "";
+	}
+
+	
+	const setupPage = () => {
+		createNewToDoForm();
+	}
+
+
+	const createNewToDoForm = () => {
+			console.log("Hi from createNewToDoForm in domCtrl.js");
+			let root_element = document.getElementById("content");
+			let form_parent_div = document.createElement("div");
+			let form_body = document.createElement("input");
+			let form_category = document.createElement("input");
+			let form_add_button = document.createElement("button");
+			form_add_button.innerText = "Add ToDo";
+			form_add_button.id = "new_todo_button";
+			form_body.id = "new_todo_body";
+			form_category.id = "new_todo_category";
+			form_parent_div.appendChild(form_body);
+			form_parent_div.appendChild(form_category);
+			form_parent_div.appendChild(form_add_button);
+			root_element.appendChild(form_parent_div);
+		}
+
+
+
+
+	return { init };
 })();
 export { domCtrl };
--- src/todoCtrl.js
+++ src/todoCtrl.js
@@ -1,56 +1,63 @@
 import { domCtrl }  from './domCtrl';
 
 const todoCrtl = (() => {
+  // This represents the todo list
   let toDoList;
 
+  // Represents a single todo object
   class ToDo{
       constructor(body, category, completed = false){
-        this.body = body;
-        this.category = category;
-        this.completed = completed;
+        this.body = body; // string
+        this.category = category; // string
+        this.completed = completed; // boolean
       }
-
-    getBody(){
-			return this.body;
-		}
-
-		getCompleted(){
-			return this.completed;
-		}
-
-		getCategory(){
-			return this.category;
-		}
-
-		setBody(body){
-			 this.body = body;
-		}
-
-		setCompleted(completed){
-			this.completed = completed;
-		}
-
-		setCategory(category){
-			this.category = category;
-		}
   }
 
+  // Hold all the todo objects
   class ToDoList{
     constructor(list){
-      this.toDoList = list;
+      this.toDoList = list; // array
+    }
+
+    // Adding a todo element to the this list
+    addToDo(todo){
+      this.toDoList.push(todo);
+    }
+
+    // Save the toDoList array to local storage
+    save(){
+      localStorage.setItem('toDoList', JSON.stringify(this.toDoList));
+      // TODO Refresh the dom because we just did an update
+      // TODO Re-attach the event listeners to the dom
     }
   }
 
+  // call this function when starting the web app
 	const init = () => {
 	   console.log("Hi from init in todoCtrl.js");
+     // Check if there is something in local storage
+     if (localStorage.toDoList) {
+       // Extract todo list array
+       const storedToDoArr = JSON.parse(localStorage.getItem('toDoList'));
+       // Create new ToDoList object using the array
+       toDoList = new ToDoList(storedToDoArr);
+     }else {
+       // Create new ToDoList object with an empty array, so no todo's yet.
+       toDoList = new ToDoList([]);
+       // Save the toDoList object to local storage
+       toDoList.save();
+     }
+
+     // Call on the dom controller to initialize the page
+    domCtrl.init(toDoList.toDoList);
+    attachEventListeners();
 	};
 
+  // Setup all the events for all the buttons on the page
   const attachEventListeners = () => {
-
+    // TODO Finish this
   }
 
-	return {
-		init
-	};
+	return { init };
 })();
 export { todoCrtl };
